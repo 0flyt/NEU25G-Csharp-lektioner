7ada14bc9b7410b9daa8cd03edb891f0db4acd51
0flyt <christopher.rydberg@gmail.com>
2025-11-03 12:00:30 +0100

    Dialogs

---
+++ L047_Dialogs_and_Windows/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L047_Dialogs_and_Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<User> Users { get; set; } = new ObservableCollection<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        private void ShowMessageBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a message!", "Message to user:");
+        }
+
+        private void AddCharacterButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to add a character to the button text?", "Update button text?", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                AddCharacterButton.Content += "?";
+            }
+        }
+
+        private void DisabledButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to disable this button?", "Disable button", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.OK)
+            {
+                DisabledButton.IsEnabled = false;
+            }
+        }
+
+        private void OpenFileDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.ShowHiddenItems = true;
+            //dialog.FileName = "Fredrik.txt";
+            //dialog.InitialDirectory = @"C:\Windows";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (dialog.ShowDialog() == true)
+            {
+                OpenFileDialogButton.Content = dialog.FileName;
+            }
+        }
+
+        private void SaveFileDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+
+            dialog.ShowHiddenItems = true;
+            dialog.FileName = "SaveFileCSharpDemo.txt";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, AddCharacterButton.Content.ToString());
+            }
+        }
+
+        private void AddUserDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new AddUserDialog();
+
+            dialog.FirstName = "<FirstName>";
+
+            if (dialog.ShowDialog() == true)
+            {
+                Users.Add(new User() { FirstName = dialog.FirstName, LastName = dialog.LastName });
+            }
+
+        }
+    }
+}
